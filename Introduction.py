85e75767f92647e56f8a0cf4d4130ababf7fbd33
mshyr <mshyrshaary@gmail.com>
2025-02-24 16:39:20 +0500

    Home Project

---
+++ Introduction.py
@@ -0,0 +1,7 @@
+n_name = (input("Please Enter Your Full Name ")).title()
+n_age = (input("Please Enter Your Age "))
+n_job = (input("Please Enter Your Job ")).title()
+print(f"Hello {n_name} You are {n_age} Years old and You are working in IT Field as {n_job}")
+
+
+print("my name {} aaaa {} aaaa {} ".format(n_name,n_age,n_job))
