059d19531998781f2415e8f0b0a132da2c730d2f
mshyr <100115242+mshyr@users.noreply.github.com>
2025-02-25 17:02:25 +0500

    Introduction_Format.py

--- Introduction.py
+++ Introduction.py
@@ -1,7 +1,7 @@
 n_name = (input("Please Enter Your Full Name ")).title()
 n_age = (input("Please Enter Your Age "))
 n_job = (input("Please Enter Your Job ")).title()
-print(f"Hello {n_name} You are {n_age} Years old and You are working in IT Field as {n_job}")
+# print(f"Hello {n_name} You are {n_age} Years old and You are working in IT Field as {n_job}")
 
 
-print("my name {} aaaa {} aaaa {} ".format(n_name,n_age,n_job))
+print("Hello My Name is {} I am {} Years old and i am working in IT Field as {} ".format(n_name,n_age,n_job))
