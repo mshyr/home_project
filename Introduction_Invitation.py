ef8d50601c9ac345212644bc76c95737285f9a03
mshyr <100115242+mshyr@users.noreply.github.com>
2025-02-25 17:27:10 +0500

    Update Introduction_Invitation.py

--- Introduction_Invitation.py
+++ Introduction_Invitation.py
@@ -1,6 +1,7 @@
 n_name = (input("Please Enter Your Full Name ")).title()
 n_batch = (input("Please Enter Your Batch Number "))
 n_year = (input("Please Enter Course Year "))
+n_city = (input("Please Enter City ")).title()
 
 
 
@@ -8,4 +9,4 @@ n_year = (input("Please Enter Course Year "))
 print(f"\nSubject : Admission Offer Letter \n\nDear {n_name},\n\nCongratulations ! \nWe are pleased to inform you that you have been granted admission to PIAIC Course Agentic AI Learning in Batch {n_batch}. \nYour student ID is PIAIC1234, and your classes are scheduled to begin in December {n_year}. \n\nIf you have any questions, feel free to contact our admissions office at Phone Number or Email Address.")
 
 
-print(f"\n\nBest regards,\nPIAIC \nAir University, Islamabad")
+print(f"\n\nBest regards,\nPIAIC \nAir University, {n_city}")
